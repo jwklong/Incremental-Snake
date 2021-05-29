b23e641cc56341887e54fea247af5be811a69a68
jwklong <jwklong@gmx.com>
2021-05-29 11:57:47 +0100

    v0.02.1

--- snake/snake.js
+++ snake/snake.js
@@ -82,7 +82,7 @@ function Snake() {
     for (var i=0; i<this.tail.length; i++) {
       if (this.x === this.tail[i].x &&
         this.y === this.tail[i].y) {
-        this.total2 += Math.round((this.total * this.multi) + ((this.multi2 * this.total)));
+        this.total2 += Math.round((this.total * this.multi) * ((this.multi2 * this.total)));
         this.total = 0;
         this.tail = [];
       }
